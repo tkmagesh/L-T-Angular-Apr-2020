dd0cbdfeb6f4bf2bb9ae4c6579eba68f5424cf44
Magesh Kuppan <tkmagesh77@gmail.com>
2020-04-30 09:59:46 +0530

    handling events

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+
+  constructor(){
+    /* setTimeout(() => {
+      this.title = 'My Angular App';
+    }, 10000); */
+  }
+
+  changeTitle(){
+    //console.log('The title is about to be changed');
+    this.title = 'My New App'
+  }
 }
