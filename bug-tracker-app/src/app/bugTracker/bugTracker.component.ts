b1aa3af00fb45cc8f8d7972580a31fc9f1636271
Magesh Kuppan <tkmagesh77@gmail.com>
2020-05-04 12:59:48 +0530

    Displaying closedCount, removing individual bugs & removing closed bugs

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -7,6 +7,7 @@ import { Bug } from './models/Bug';
 })
 export class BugTrackerComponent{
     bugs : Bug[] = [];
+    //closedCount : number = 0;
 
     onAddNewClick(bugName : string){
         const newBug: Bug = { name : bugName , isClosed :false};
@@ -14,6 +15,19 @@ export class BugTrackerComponent{
     }
 
     onBugNameClick(bugToToggle : Bug){
-        console.log(bugToToggle);
+        bugToToggle.isClosed = !bugToToggle.isClosed;
+        //this.closedCount = this.bugs.reduce((result, bug) => bug.isClosed ? ++ result : result, 0);
+    }
+
+    onRemoveClick(bugToRemove : Bug){
+        this.bugs = this.bugs.filter(bug => bug !== bugToRemove);
+    }
+
+    onRemoveClosedClick() : void {
+        this.bugs = this.bugs.filter(bug => !bug.isClosed);
+    }
+
+    getClosedCount() : number {
+        return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result, 0);
     }
 }
