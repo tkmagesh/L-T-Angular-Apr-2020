65ff08d19e0fcfd10097f8f47368efe87f7c17b3
Magesh Kuppan <tkmagesh77@gmail.com>
2020-05-08 11:55:06 +0530

    refactoring to utils module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { UtilsModule } from './utils/utils.module';
+
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 import { BugEditComponent } from './bugTracker/views/bugEdit.component';
 
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
-import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
@@ -28,12 +27,10 @@ function localStorageFactory(){
     , BugStatsComponent
     , BugEditComponent
     , ClosedCountPipe
-    , TrimTextPipe
-    , SortPipe
-    , ElapsedPipe
   ],
   imports: [
     BrowserModule
+    , UtilsModule
   ],
   /* providers: [
     BugOperationsService
